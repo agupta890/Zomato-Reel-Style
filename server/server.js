b3bc2eb7ca7a6bb2dbd7028ab8ac6b6a5c2ef42a
Ansh Gupta <ng607301@gmail.com>
2026-03-03 19:31:13 +0530

    cors policy

--- server/server.js
+++ server/server.js
@@ -4,7 +4,13 @@ const authRoutes = require("./routes/auth-routes");
 const foodRoutes = require("./routes/food-routes");
 const connectDB = require("./db/connection");
 const cookieParser = require("cookie-parser");
+const cors = require('cors');
 const app = express();
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
