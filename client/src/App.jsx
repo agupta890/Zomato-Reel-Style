fa7f3b352eded006c245a7b748f464631100f9d2
Ansh Gupta <ng607301@gmail.com>
2026-03-03 18:48:58 +0530

    frontend push

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import './App.css'
+import './styles/theme.css'
+import AppRoutes from './Routes/AppRoutes'
+
+function App() {
+
+
+  return (
+    <>
+      <AppRoutes />
+    </>
+  )
+}
+
+export default App
